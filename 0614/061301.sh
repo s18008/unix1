fc970f7ba44e2fe938f6daf4a18ab67f40cf9504
shion shukumine <s18008@std.it-college.ac.jp>
2018-06-14 14:27:26 +0000

    6月14日の課題

--- 0614/061301.sh
+++ 0614/061301.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
-
-
+val=$1
+expr $val + 1 > /dev/null 2>&1
+re=$?
+if [ $re -gt 1 ]; then
+	echo '数値を入力してください'
+  exit 1
+fi
 
 if [ "$(expr $1 % 4)" -eq 0 -a "$(expr $1 % 100)" -ne 0 ]; then
 	echo 'うるう年です'
